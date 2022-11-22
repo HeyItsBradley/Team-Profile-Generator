3a43b67e4b244d8a6e5ddd01db51486df2901c2e
HeyItsBradley <bradleyrodriguez77@gmail.com>
2022-11-22 00:05:10 -0700

    added the functions to appened the first manager card to an html file

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
-const { off } = require("process");
 
 class Employee {
   constructor(name, id, email, className) {
@@ -241,9 +240,53 @@ function internStart() {
   });
 }
 
+function writeToFile(fileName, data) {
+  fs.writeFile(`./dist/${fileName}`, data, (err) => {
+    err ? console.error(err) : console.log("success!");
+  });
+}
+
+function generateHTML(data) {
+  return `
+  <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Team Profile Generator</title>
+    <!-- CSS only -->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
+      crossorigin="anonymous"
+    />
+  </head>
+  <body>
+    <h1 class="text-center bg-primary p-3">My Team</h1>
+    <main id="main">
+      <div class="bg-secondary">
+        <h2>${data.name}</h2>
+        <h3>${data.className}</h3>
+        <p>ID:${data.id}</p>
+        <p>Email:${data.email}</p>
+        <p>Office Number:${data.officeNumber}</p>
+      </div>
+    </main>
+    <!-- JavaScript Bundle with Popper -->
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
+`;
+}
+
 function init() {
   inquirer.prompt(startQuestions).then((Response) => {
-    console.log(Response);
     const manager = new Manager(
       Response.managerName,
       Response.managerId,
@@ -252,6 +295,9 @@ function init() {
       Response.managerOffice
     );
     console.log(manager);
+    let madeHTML = generateHTML(manager);
+    writeToFile("example.html", madeHTML);
+
     if (Response.menuSelect === "Finish Build") {
       return;
     } else if (Response.menuSelect === "Add an engineer") {
